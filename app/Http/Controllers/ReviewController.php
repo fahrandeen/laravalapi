81416069b31a707e95dfaba6015e871fbbeb5c82
Fahran Deen <fahrandeen17@gmail.com>
2020-07-08 15:15:50 +0530

    Review Resource & Product Pagination

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Model\Review;
 use App\Model\Product;
 use Illuminate\Http\Request;
+use App\Http\Resources\ReviewResource;
 
 class ReviewController extends Controller
 {
@@ -15,8 +16,8 @@ class ReviewController extends Controller
      */
     public function index(Product $product)
     {
-        return \App\Http\Resources\ReviewResource::collection($product->reviews)
-//        return $product->reviews();
+        return ReviewResource::collection($product->reviews);
+//        return $product->reviews;
 //        return Review::all();
     }
 
--- app/Http/Resources/ReviewResource.php
+++ app/Http/Resources/ReviewResource.php
@@ -14,6 +14,12 @@ class ReviewResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return[
+            'customer' => $this->customer,
+            
+            'body' => $this->review,
+            
+            'star' => $this->star
+        ];
     }
 }
