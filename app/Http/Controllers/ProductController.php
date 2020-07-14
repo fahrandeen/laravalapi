c20b44586ce17008707af726e06e23a712f456c3
Fahran Deen <fahrandeen17@gmail.com>
2020-07-14 12:45:09 +0530

    Product Updated

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,27 +1,26 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Http\Requests\ProductRequest;
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
 use App\Model\Product;
 use Illuminate\Http\Request;
 
-class ProductController extends Controller
-{
-    
+class ProductController extends Controller {
+
     public function __construct() {
-        
+
         $this->middleware('auth:api')->except('index', 'show');
-        
     }
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
+    public function index() {
         return ProductCollection::collection(Product::paginate(5));
     }
 
@@ -30,8 +29,7 @@ class ProductController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
-    {
+    public function create() {
         
     }
 
@@ -41,20 +39,19 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(ProductRequest $request)
-    {
+    public function store(ProductRequest $request) {
         $product = new Product;
-        $product->name = $request-> name;
-        $product->detail = $request-> description;
-        $product->stock = $request-> stock;
-        $product->price = $request-> price;
-        $product->discount = $request-> discount;
-        
-        $product-> save();
-        
+        $product->name = $request->name;
+        $product->detail = $request->description;
+        $product->stock = $request->stock;
+        $product->price = $request->price;
+        $product->discount = $request->discount;
+
+        $product->save();
+
         return response([
             'data' => new ProductResource($product)
-        ], 201);
+                ], 201);
     }
 
     /**
@@ -63,8 +60,7 @@ class ProductController extends Controller
      * @param  \App\Model\Product  $product
      * @return \Illuminate\Http\Response
      */
-    public function show(Product $product)
-    {
+    public function show(Product $product) {
         return new ProductResource($product);
     }
 
@@ -74,8 +70,7 @@ class ProductController extends Controller
      * @param  \App\Model\Product  $product
      * @return \Illuminate\Http\Response
      */
-    public function edit(Product $product)
-    {
+    public function edit(Product $product) {
         //
     }
 
@@ -86,9 +81,20 @@ class ProductController extends Controller
      * @param  \App\Model\Product  $product
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Product $product)
-    {
-        //
+    public function update(Request $request, Product $product) {
+        $request['description'] = $request->description;
+
+        unset($request['description']);
+
+        $product->update($request->all());
+
+        return response([
+//            'data' => new ProductResource($product)
+//                ], Response::HTTP_CREATED);
+//
+//        return response([
+            'data' => new ProductResource($product)
+                ], 201);
     }
 
     /**
@@ -97,8 +103,8 @@ class ProductController extends Controller
      * @param  \App\Model\Product  $product
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Product $product)
-    {
+    public function destroy(Product $product) {
         //
     }
+
 }
